1e0eaab045f2b956539c7d88aa79b6e63c9c3b54
generyand <generyan.dep@gmail.com>
2024-05-02 14:30:42 +0800

    gamay ra na learn

--- src/components/ViewBasedAnimations.jsx
+++ src/components/ViewBasedAnimations.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 export default function ViewBasedAnimations() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  // const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    console.log("Is in view -> ", isInView);
+  }, [isInView]);
+
   return (
-    <div className="flex flex-col gap-4 mx-auto text-white mt-96 w-fit">
-      <motion.div
+    <div className="flex flex-col w-full mx-auto text-white">
+      {/* <motion.div
         initial={{ opacity: 0, y: "40px" }}
         whileInView={{ opacity: 1, y: "0px" }}
         transition={{ duration: 0.5 }}
@@ -14,7 +22,15 @@ export default function ViewBasedAnimations() {
         reiciendis repellendus odio aspernatur quaerat aut earum molestiae, quod
         illum fugit corrupti voluptatum? Dolorem a quo sint dicta dolorum non
         perspiciatis?
-      </motion.div>
+      </motion.div> */}
+      <div
+        className={
+          isInView
+            ? "bg-blue-800 w-full transition h-80 rounded-lg"
+            : "bg-red-700 w-full transition h-80"
+        }
+      />
+      <div ref={ref} className="w-full transition bg-green-700 h-80" />
     </div>
   );
 }
